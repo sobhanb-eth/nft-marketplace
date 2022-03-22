4e34ac4feac254b309f02a7567460894164345f6
ViperTechnologies-RnD <sobhan.bahrami@metu.edu.tr>
2022-03-22 21:51:49 +0100

    Add Project Setup, NFT and Marketplace Contract

---
+++ src/backend/contracts/Marketplace.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Marketplace is ReentrancyGuard {
+
+    //State Variables
+    address payable public immutable feeAccount; // the account that recieves fees
+    uint public immutable feePercent; // the fee percentage on sales
+    uint public itemCount;
+
+    struct Item {
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+
+    event Offered (
+        uint itemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller
+    );
+
+    // itemId -> Item
+    mapping(uint => Item) public items;
+
+    constructor(uint _feePercent) {
+        feeAccount = payable(msg.sender);
+        feePercent = _feePercent;
+    }
+
+    function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
+        
+        require(_price > 0, "Price must be greater than zero");
+        // increament itemCount
+        itemCount++;
+        // transfer nft
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        // add new item to items mapping
+        items[itemCount] = Item (
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+        // emit Offered event
+        emit Offered (
+            itemCount,
+            address(_nft),
+            _tokenId,
+            _price,
+            msg.sender
+        );
+        
+    }
+}
---
+++ src/backend/test/NFTMarketplace.test.js
@@ -0,0 +1,77 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const toWei = (num) => ethers.utils.parseEther(num.toString()) // 1 ether == 10**18 wei
+const fromWei = (num) => ethers.utils.formatEther(num)
+ 
+describe("NFTMarketplace", function(){
+    let deployer, addr1, addr2, nft, marketplace
+    let feePercent = 1
+    let URI = "Sample URI"
+    beforeEach(async function (){
+        // Get contract factories
+        const NFT = await ethers.getContractFactory("NFT");
+        const Marketplace = await ethers.getContractFactory("Marketplace");
+        // Get signers
+        [deployer, addr1, addr2] = await ethers.getSigners()
+        // Deploy Contracts
+        nft = await NFT.deploy();
+        marketplace = await Marketplace.deploy(feePercent);
+    });
+    describe("Deployment", function (){
+        it("should track name and symbol of the nft collection", async function () {
+            expect(await nft.name()).to.equal("DApp NFT")
+            expect(await nft.symbol()).to.equal("DAPP")
+
+        });
+        it("should track feeAccount and feePercent of the marketplace", async function () {
+            expect(await marketplace.feeAccount()).to.equal(deployer.address);
+            expect(await marketplace.feePercent()).to.equal(feePercent);
+        });
+    })
+    describe("Minting NFTs", function () {
+        it("Should track each minted NFT", async function() {
+            //addr1 mints an NFT
+            await nft.connect(addr1).mint(URI)
+            expect(await nft.tokenCount()).to.equal(1);
+            expect(await nft.balanceOf(addr1.address)).to.equal(1);
+            expect(await nft.tokenURI(1)).to.equal(URI);
+
+            //addr2 mints an NFT
+            await nft.connect(addr2).mint(URI)
+            expect(await nft.tokenCount()).to.equal(2);
+            expect(await nft.balanceOf(addr2.address)).to.equal(1);
+            expect(await nft.tokenURI(2)).to.equal(URI);
+        })
+    }) 
+    describe("Minting NFTs", function () {
+        beforeEach(async function () {
+            // addr1 mints an nft
+            await nft.connect(addr1).mint(URI)
+            // addr1 approves marketplace to spend nft
+            await nft.connect(addr1).setApprovalForAll(marketplace.address, true)
+        })
+        it("Should track newly created item. transfer NFT from seller to marketplace and emit Offered event", async function () {
+            // addr1 offers their nft at a price of 1 ether
+            await expect(marketplace.connect(addr1).makeItem(nft.address, 1, toWei(1))).to.emit(marketplace, "Offered").withArgs(1, nft.address, 1, toWei(1),addr1.address)
+
+            // Owner of NFT should now be the marketplace
+            expect(await nft.ownerOf(1)).to.equal(marketplace.address);
+            // Item count should now equal to 1
+            expect(await marketplace.itemCount()).to.equal(1)
+            // Get item from items mapping then check fields to ensure they are correct
+            const item = await marketplace.items(1)
+            expect(item.itemId).to.equal(1)
+            expect(item.nft).to.equal(nft.address)
+            expect(item.tokenId).to.equal(1)
+            expect(item.price).to.equal(toWei(1))
+            expect(item.sold).to.equal(false)
+
+        });
+        it("Should fail if price is set to zero", async function () {
+            await expect(
+                marketplace.connect(addr1).makeItem(nft.address, 1, 0)
+            ).to.be.revertedWith("Price must be greater than zero");
+        });
+    });
+})
